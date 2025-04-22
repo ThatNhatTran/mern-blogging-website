bf797865205af98d8837aca1cfc845eadeea1c8b
dtran1301 <ntra0028@student.monash.edu>
2025-04-23 09:24:13 +1000

    update logo

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -1,6 +1,30 @@
+import logo from "../imgs/logo.png";
+import {Link} from "react-router-dom";
+
 const Navbar = () => {
     return (
-        <h1>Navbar</h1>
+        // the href to redirect to the mainpage và cái src là cái hình lá. THAY ĐỔI SAU KHI XONG CLIP
+        <nav className="navbar">
+            <Link to="/" className="flex-none w-10">
+                <img src={logo} className="w-full" />          
+            </Link>
+
+            <div className="absolute 
+                            bg-white 
+                            w-full 
+                            left-0 
+                            top-full 
+                            mt-0.5 
+                            border-b 
+                            border-grey
+                            py-4 
+                            px-[5vw]">
+                <input type="text" 
+                    placeholder="Search" 
+                    className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey"/>
+
+            </div>
+        </nav>
     )
 }
 
