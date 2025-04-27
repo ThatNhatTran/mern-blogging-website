99db69d6ef68833c1e0c0a3d4f51fc31715f75b2
dtran1301 <ntra0028@student.monash.edu>
2025-04-28 09:41:31 +1000

    update nav bar

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 
 const Navbar = () => {
     return (
-        // the href to redirect to the mainpage và cái src là cái hình lá. THAY ĐỔI SAU KHI XONG CLIP
+        // the href to redirect to the mainpage và cái src là cái hình lá. THAY ĐỔI SAU KHI XONG CLIP. Cái Icon nó không hoạt động 
         <nav className="navbar">
             <Link to="/" className="flex-none w-10">
                 <img src={logo} className="w-full" />          
@@ -18,10 +18,18 @@ const Navbar = () => {
                             border-b 
                             border-grey
                             py-4 
-                            px-[5vw]">
+                            px-[5vw]
+                            md:border-0
+                            md:block md:relative md:iset-0 md:p-0 md:w-auto">
                 <input type="text" 
                     placeholder="Search" 
-                    className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey"/>
+                    className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12"/>
+                <i className= "fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i> 
+                <div className="flex items-center gap-3 md:gap-6 ml-auto">
+                    <button>
+                        <i className="fi fi-rr-search"></i>
+                    </button>
+                </div>
 
             </div>
         </nav>
@@ -29,3 +37,4 @@ const Navbar = () => {
 }
 
 export default Navbar;
+
